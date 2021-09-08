b7ef31418e9764c4ef719557aa77aabb9ce4ab80
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-09-08 11:51:18 +0700

    setup dir, go mod, route

---
+++ app/server.go
@@ -0,0 +1,34 @@
+package app
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+	"github.com/jinzhu/gorm"
+)
+
+type Server struct {
+	DB     *gorm.DB
+	Router *mux.Router
+}
+
+func (server *Server) Initialize() {
+	fmt.Println("Welcome to GoShop")
+
+	server.Router = mux.NewRouter()
+	server.initializeRoutes()
+}
+
+func (server *Server) Run(addr string) {
+	fmt.Println("Listening to port", addr)
+	log.Fatal(http.ListenAndServe(addr, server.Router))
+}
+
+func Run() {
+	var server = Server{}
+
+	server.Initialize()
+	server.Run(":9000")
+}
---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "github.com/adhiardiansyah/goshop/app"
+
+func main() {
+	app.Run()
+}
