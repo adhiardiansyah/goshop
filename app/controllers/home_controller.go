12f96bc9cbe55e93af3eaaf210c49d40e22415a6
Adhi Ardiansyah <adhiardiansyah23@gmail.com>
2021-09-09 17:32:28 +0700

    change tmpl to html

--- app/controllers/home_controller.go
+++ app/controllers/home_controller.go
@@ -1,18 +1,35 @@
 package controllers
 
 import (
+	"html/template"
 	"net/http"
-
-	"github.com/unrolled/render"
+	// "github.com/unrolled/render"
 )
 
-func Home(w http.ResponseWriter, r *http.Request) {
-	render := render.New(render.Options{
-		Layout: "layout",
-	})
+var tpl *template.Template
 
-	_ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
-		"title": "Home Title",
-		"body":  "Home Description",
-	})
+func ParseTemplates() (*template.Template, error) {
+
+	if t, _ := tpl.ParseGlob("templates/*.html"); t != nil {
+		tpl = t
+	}
+	if t, _ := tpl.ParseGlob("templates/pages/*.html"); t != nil {
+		tpl = t
+	}
+	return tpl.ParseGlob("/*.html")
+}
+
+func Home(w http.ResponseWriter, r *http.Request) {
+	// render := render.New(render.Options{
+	// 	Layout: "layout",
+	// })
+
+	// _ = render.HTML(w, http.StatusOK, "home", map[string]interface{}{
+	// 	"title": "Home Title",
+	// 	"body":  "Home Description",
+	// })
+
+	// tpl, _ = tpl.ParseGlob("templates/*.html")
+	ParseTemplates()
+	tpl.ExecuteTemplate(w, "home.html", nil)
 }
--- go.mod
+++ go.mod
@@ -9,7 +9,6 @@ require (
 	github.com/gosimple/slug v1.10.0
 	github.com/joho/godotenv v1.3.0
 	github.com/shopspring/decimal v1.2.0
-	github.com/unrolled/render v1.4.0
 	github.com/urfave/cli v1.22.5
 	gorm.io/driver/mysql v1.1.2
 	gorm.io/gorm v1.21.14
@@ -17,12 +16,10 @@ require (
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
-	github.com/fsnotify/fsnotify v1.4.9 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
 	github.com/gosimple/unidecode v1.0.0 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.2 // indirect
 	github.com/russross/blackfriday/v2 v2.0.1 // indirect
 	github.com/shurcooL/sanitized_anchor_name v1.0.0 // indirect
-	golang.org/x/sys v0.0.0-20210525143221-35b2ab0089ea // indirect
 )
